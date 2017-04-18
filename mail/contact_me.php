26a0dc146cc2e6efb48761f6ca890aa084959a17
jasontsemf <jasontsemf@gmail.com>
2017-04-19 05:29:06 +0800

    just email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,6 +21,7 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: jasontsemf@gmail.com\n"; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE MESSAGE TO BE FROM*
 $headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body);
+//,$headers);
 return true;
 ?>

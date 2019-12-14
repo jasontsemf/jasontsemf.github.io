5a16c6165ff433f848705488e688c891487ca3ae
Jason <jason1996429@gmail.com>
2019-12-14 06:07:11 -0500

    restructure

--- assets/js/loading.js
+++
@@ -1,5 +0,0 @@
-// Wait for window load
-$(document).ready(function() {
-  // Animate loader off screen
-  $(".se-pre-con").fadeOut("slow");
-});
---
+++ backup/assets/js/loading.js
@@ -0,0 +1,5 @@
+// Wait for window load
+$(document).ready(function() {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");
+});
